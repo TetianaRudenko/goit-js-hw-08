3a3ebf67f2367912a9159285998857aa8f701b2d
TetianaRudenko <fyilibratgif@gmail.com>
2023-03-10 13:22:47 +0200

    add the condition for alert

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -7,31 +7,32 @@ const refs = {
   message: document.querySelector('textarea[name="message"]'),
 };
 
+//ключ для сховища
 const STORAGE_KEY = 'feedback-form-state';
-let formData = {
-  email: '',
-  message: '',
-};
+let formData = {};
 
 populatedMessage();
 
 //додати прослуховувач
 refs.form.addEventListener('submit', onFormSubmit);
-refs.form.addEventListener('input', throttle(onTextareaInput, 1000));
+refs.form.addEventListener('input', throttle(onTextareaInput, 500));
 
 //Зупиняємо поведінку за замовчуванням   e.preventDefault();
 //Прибираємо повідомлення із сховища  localStorage.removeItem(STORAGE_KEY)
 //очищаємо форму refs.form.reset();
+// +++ порожні поля Не відправляти, а виводити alert()
 function onFormSubmit(e) {
   e.preventDefault();
    
-    if (refs.email.value === '' || refs.message.value === '') {
+    if (refs.email.value === '') {
       return alert('Заповніть, будь ласка, необхідні поля');
+    } else if (refs.message.value === '') {
+      return alert("Ваш коментар може змінити світ");
+    } else {
+      e.target.reset(); 
+      localStorage.removeItem(STORAGE_KEY); 
+      console.log(formData);
     }
-  
-  e.target.reset(); 
-  localStorage.removeItem(STORAGE_KEY); 
-  console.log(formData);
 }
 
 //Отримуємо значення поля
@@ -42,6 +43,7 @@ function onTextareaInput() {
   formData[refs.message.name] = refs.message.value;
 
   localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+  
 }
 
 
@@ -55,4 +57,5 @@ function populatedMessage() {
     refs.email.value = savedFormData.email;
   }
 }
+
 
